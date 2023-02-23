ae3e4f59be7df318dc958d98a9bc62150c83d43b
Arvö <73062084+arvofm@users.noreply.github.com>
2023-02-23 19:30:39 +0000

    Update keybinds.lua
    Changed tabs movement / navigation

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -49,8 +49,8 @@ map('n', 'W', '$')
 -- tabs movement
 local opts = { noremap = true, silent = true }
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-,>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
 map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
