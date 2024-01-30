35416bef6de0700f3b670fd061cb0a6d51ccedad
Arvö <73062084+arvofm@users.noreply.github.com>
2024-01-30 01:21:16 +0000

    Add files via upload

---
+++ .config/nvim/init.lua
@@ -0,0 +1,44 @@
+---------------------------
+-- a neat neovim config
+-- by arvo
+---------------------------
+
+-- globals
+require('plugins')
+require('keybinds')
+require('settings')
+
+-- colorscheme
+-- (gruvbox-material / nightfox-duskfox / rosepine / kanagawa-colorscheme / real-gruvbox / phoenix)
+require('arvosNvim')
+
+-- better syntax highlighting
+require('treesitter')
+
+-- bottom status line
+require('lualine-status')
+
+-- better tabs
+require('barbartabs')
+
+-- side panel file explorer
+require('nvimtree-explorer')
+
+-- colorize colors
+require('colorizecolors')
+
+-- html / css support
+require('emmethtml')
+
+-- IDE stuff
+require('lsp')
+
+-- LaTeX syntax
+require('vimtex')
+
+-- snippets
+require('ultisnips')
+
+-- finally, all my own
+require('commands')
+
