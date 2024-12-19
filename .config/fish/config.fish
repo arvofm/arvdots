f09c46211c60f4477a98354f2c2dbf23eba5b93d
Arvö <73062084+arvofm@users.noreply.github.com>
2024-12-19 23:58:25 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,37 +39,34 @@ function __zathura
 end
 
 # alias
-alias ls='lsd --group-directories-first'
-alias la='ls -a'
-alias ll='ls -l'
-alias rm='rm -vrf'
-alias mv='mv -v'
-alias cp='rsync -rphv'
-alias mkdir='mkdir -pv'
-alias grep='grep --color=auto -i'
-alias du='dust'
-alias cat='bat'
-alias imv='imv -f'
-alias speedtest='speedtest --secure'
+alias ls='lsd -F --group-directories-first --icon never'
+abbr la 'ls -a'
+abbr ll 'ls -l'
+abbr rm 'rm -vrf'
+abbr mv 'mv -v'
+abbr cp 'rsync -rphv'
+abbr mkdir 'mkdir -pv'
+abbr grep 'grep --color=auto -i -nH --null -e'
+abbr du 'dust'
+abbr cat 'bat'
+abbr imv 'imv -f'
+abbr speedtest 'speedtest --secure'
 alias zathura='__zathura'
 abbr q 'exit'
 abbr sn 'sudo poweroff'
 abbr rb 'sudo reboot'
 abbr cg 'cd ~/.config'
 abbr xi 'sudo xbps-install -Su'
-abbr xq 'xbps-query -l | grep'
+abbr xq 'xbps-query -s'
 abbr xs 'xbps-query -Rs'
 abbr xr 'sudo xbps-remove -R'
 abbr vim 'nvim'
 abbr nvim 'nvim --clean'
 abbr tarc 'tar -czvf'
 abbr tarx 'tar -xvzf'
-abbr tarl 'tar -tvf'
-abbr win32 'WINEPREFIX=~/.new32prefix WINEARCH="win32"'
-alias ytdlpv="yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail --embed-subs --write-subs -f 'bv*+ba'"
-alias ytdlpa="yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail --embed-subs --write-subs -f 'ba'"
-alias ytdlppv="yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail --embed-subs --write-subs --download-archive videos.txt -f 'bv*+ba'"
-alias ytdlppa="yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail --embed-subs --write-subs --download-archive videos.txt -f 'ba'"
+abbr tarl 'tar -tzvf'
+abbr ytdlpv "yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail --embed-subs --write-subs -f 'bv*[height=1080]+ba/b[height=1080] / bv*+ba/b'"
+abbr ytdlpa "yt-dlp --external-downloader=aria2c --embed-metadata --embed-thumbnail -f 'ba' --remux-video mkv"
 abbr webcam 'mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
 abbr ffaudiorecord 'ffmpeg -f pulse -i 1'
 abbr ffaudiocut 'ffmpeg -i input.m4a -ss 00:00:00 -t 00:00:10 -acodec copy output.m4a'
